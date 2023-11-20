8941fe9d98132665d4b4b33c28b65c594bf484f0
Dan O'Donovan <dan.odonovan@gmail.com>
2023-11-20 21:03:04 +0000

    build(deps): add version pins

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ import setuptools
 
 setuptools.setup(
     name="parquet_athena",
-    version='0.1',
-    description='Package for generating athena table schema for parquet ',
-    url='https://bitbucket.org/aa-scm-admin/misc', 
-    packages=['parquet_to_athena'],
-    scripts = ['scripts/parquet_to_athena'],
-    install_requires=['pyarrow', 'awscli', 's3fs', 'query-athena @ git+https://github.com/affinityanswers/query-athena.git@64d2d46270fac135e0b4dbcc533f1f440c0f614c']
+    version="0.2",
+    description="Package for generating athena table schema for parquet ",
+    url="https://github.com/affinityanswers/schema_from_parquet",
+    packages=["parquet_to_athena"],
+    scripts=["scripts/parquet_to_athena"],
+    install_requires=[
+        "pyarrow<15,>=14",
+        "awscli<1.31>=1.0",
+        "s3fs<0.5,>=0.4",
+        "query-athena @ git+https://github.com/affinityanswers/query-athena.git@64d2d46270fac135e0b4dbcc533f1f440c0f614c",
+    ],
 )
